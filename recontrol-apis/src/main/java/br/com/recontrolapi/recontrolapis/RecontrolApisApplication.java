a75517205fa0967ae464cdffae3dcdd14293a19e
Renata-Arbol <111192394+Renata-Arbol@users.noreply.github.com>
2022-08-21 14:31:13 -0300

    Spring teste 1
    Eu passei parametros personalizados por mim atraves do verbo "GET", ou seja, parametros na URL, exibindo valores conforme regras passadas.
    Utilizei java, framework (spring), maven (gerenciador de pacotes/bibliotecas/repositorios).
    Integer (é mais amplo que int) - é um int por orientação ao objeto*preferencialmente utilize o Integer ao inves de int EM JAVA.
    ; para concluir os comandos ao final da linha
    Teve um pouco de if.

--- recontrol-apis/src/main/java/br/com/recontrolapi/recontrolapis/RecontrolApisApplication.java
+++ recontrol-apis/src/main/java/br/com/recontrolapi/recontrolapis/RecontrolApisApplication.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @SpringBootApplication
 @RestController
 public class RecontrolApisApplication {
@@ -14,10 +16,5 @@ public class RecontrolApisApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RecontrolApisApplication.class, args);
 	}
-	//required the name
-	@RequestMapping("/")
-	@ResponseBody
-	public String helloWorld(@RequestParam String name){
-		return "Hello World from Spring Boot to you "+ name +"  "+ 38;
-	}
+
 }
---
+++ recontrol-apis/src/main/java/br/com/recontrolapi/recontrolapis/controllers/Payment.java
@@ -0,0 +1,31 @@
+package br.com.recontrolapi.recontrolapis.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Payment {
+    //required the name
+    @RequestMapping("/")
+    @ResponseBody
+    public String helloWorld(@RequestParam String name, @RequestParam Integer idade){
+        if (name.equals("renata")){
+            System.out.print("Voce nao esta entendendo nada");
+            return "Voce nao esta entendendo nada";
+        }
+        System.out.print("Hello World from Spring Boot to you "+ upperName(name) +" idade aqui "+ msgOfAge(idade) +"");
+        return "Hello World from Spring Boot to you "+ (name) +" idade "+ idade +"";
+    }
+
+    public String upperName(String name){
+        return name.toUpperCase();
+    }
+
+    public Integer msgOfAge(Integer idade){
+        return idade;
+    }
+
+
+}
